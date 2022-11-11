3af34b5a33f1ec28588c44373083914445ff26e5
Alexandr Drankevich <a.a.drankevich@gmail.com>
2022-11-11 10:17:44 +0300

    1

--- src/main/java/com/ex/library/conroller/PrintingProductController.java
+++ src/main/java/com/ex/library/conroller/PrintingProductController.java
@@ -1,11 +1,14 @@
 package com.ex.library.conroller;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,17 +16,16 @@ import javax.servlet.http.HttpServletRequest;
 import com.ex.library.entity.PrintingProduct;
 import com.ex.library.service.PrintingProductService;
 
-
 @Controller
 public class PrintingProductController {
 	@Autowired
 	private PrintingProductService printingProductService;
 
+	static final Logger logger = LogManager.getLogger((PrintingProductController.class));
+
 	@RequestMapping("/base_page")
 	public String goToBasePage(Model model) {
-
 		List<PrintingProduct> listProducts = printingProductService.findAll();
-		System.out.println(listProducts);
 		model.addAttribute("printingProducts", listProducts);
 		return "basePage";
 	}
@@ -36,7 +38,7 @@ public class PrintingProductController {
 	@RequestMapping("/delete")
 	public String delete(HttpServletRequest request) {
 		String id = request.getParameter("productId");
-		printingProductService.delete(id);
+		printingProductService.delete(Long.parseLong(id));
 		return "redirect:/base_page";
 	}
 
@@ -49,25 +51,27 @@ public class PrintingProductController {
 	}
 
 	@RequestMapping("/save")
-	public String save(HttpServletRequest request, @ModelAttribute("printingProduct") PrintingProduct printingProduct) {
+	public String save(HttpServletRequest request, @ModelAttribute("printingProduct") PrintingProduct printingProduct,
+			Model model) {
 		printingProductService.save(printingProduct);
-		return "redirect:/base_page";
-
+		List<PrintingProduct> listProducts = new ArrayList<PrintingProduct>();
+		listProducts.add(printingProductService.findById(printingProduct.getId()));
+		model.addAttribute("printingProducts", listProducts);
+		return "basePage";
 	}
-	
+
 	@RequestMapping("/searchForm")
 	public String showSearchForm() {
 		return "searchPage";
-
 	}
-	
+
 	@RequestMapping("/search")
 	public String search(HttpServletRequest request, Model model) {
-		String searchType=request.getParameter("searhType");
-		String searchData=request.getParameter("searchData");
-		List<PrintingProduct> listProducts=printingProductService.find(searchType,searchData);
+		String searchType = request.getParameter("searhType");
+		String searchData = request.getParameter("searchData");
+		List<PrintingProduct> listProducts = printingProductService.findByData(searchType, searchData);
 		model.addAttribute("printingProducts", listProducts);
-	    return "basePage";
+		logger.info(listProducts);
+		return "basePage";
 	}
-
 }
--- src/main/java/com/ex/library/dao/PrintingProductRepository.java
+++ src/main/java/com/ex/library/dao/PrintingProductRepository.java
@@ -1,5 +1,7 @@
 package com.ex.library.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.ex.library.entity.PrintingProduct;
 
 @Repository
 public interface PrintingProductRepository extends CrudRepository<PrintingProduct, Long> {
+	List<PrintingProduct> findByAuthor(String author);
+	List<PrintingProduct> findByPublisher(String publisher);
+	List<PrintingProduct> findByName(String name);
 }
